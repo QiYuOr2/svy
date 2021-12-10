e61c0bf51d19328af21487c0a973fc5934d434ac
qiyuor2 <mengyu6498@icloud.com>
2021-12-10 21:24:44 +0800

    chore: 单词拼写错误

--- src/common/utils/bulkImport.ts
+++ src/common/utils/bulkImport.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import glob from 'glob';
 import { createAction } from './createAction';
 
-export function buklImport(dir: string): ReturnType<typeof createAction>[] {
+export function bulkImport(dir: string): ReturnType<typeof createAction>[] {
   const files = glob.sync(path.join(dir, '*.ts'));
   return files.map((file) => {
     const key = /(?<=\/)(\w+)(?=\.)/.exec(file)![0];
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Command } from 'commander';
 import path from 'path';
 import pkg from '../package.json';
-import { buklImport } from './common/utils';
+import { bulkImport } from './common/utils';
 
 const program = new Command();
 
 program.version(pkg.version);
 
-buklImport(path.join(__dirname, './modules')).map((command) => {
+bulkImport(path.join(__dirname, './modules')).map((command) => {
   program.command(command.optionsStr).description(command.description).action(command.action);
 });
 
