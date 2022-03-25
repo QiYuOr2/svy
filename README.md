430bab55a94d904a240e8d43ebf8ca002d2ffd91
qiyuor2 <mengyu6498@icloud.com>
2022-03-25 23:01:56 +0800

    feat: base template

---
+++ README.md
@@ -0,0 +1,8 @@
+# SVY CLI 自用 CLI 工具
+
+使用 Rust 实现
+
+## 功能列表
+
+| Command | Options | Description |
+| ------- | ------- | ----------- |
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use clap::{Args, Parser, Subcommand};
+// use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// 切换npm源 svy registry [NAME] [--list|-l]
+    Registry(Registry),
+}
+
+#[derive(Args)]
+struct Registry {
+    /// 查询所有源
+    #[clap(short, long)]
+    list: bool,
+
+    /// 切换对应源
+    name: Option<String>,
+}
+
+impl Registry {
+    fn exec(&self) {
+        // 含有`--list`时，查询所有源
+        if self.list == true {
+            println!("registry list");
+            return;
+        }
+        // 切换源
+        if let Some(name) = &self.name {
+            println!("registry use {}", name)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Registry(list) => list.exec(),
+    };
 }
