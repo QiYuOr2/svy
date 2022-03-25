cd869cf626669541c6f7f47eb2124d48db02da88
qiyuor2 <mengyu6498@icloud.com>
2022-03-25 16:55:13 +0800

    refactor: rust init

--- src/common/constants.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import os from 'os';
-
-export const SVYRC = path.join(os.homedir(), '.svyrc');
--- src/common/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './bulkImport';
-export * from './createAction';
-export * from './checkConfig';
-
-export const isUndefined = (val: unknown) => {
-  return typeof val === 'undefined' ? true : typeof val === 'string' ? val === 'undefined' : false;
-};
